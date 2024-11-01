025a985768de33b409f44ff9904129aa269d3169
Gene Carlson <kvngncrlsn@gmail.com>
2024-11-01 22:38:43 +0900

    Updated for 22fcdba.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=becf273c2e704473af5ce2df10509600adf1068f
+COMMIT=22fcdbad37a0715091a5ea9134028512a168368b
 wget https://github.com/pghvlaans/sbotools3/archive/$COMMIT.zip
