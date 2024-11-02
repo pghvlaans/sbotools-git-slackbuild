e0da0d2ef4ee29060c9e055a95f1d7297bd74eb9
Gene Carlson <kvngncrlsn@gmail.com>
2024-11-03 00:52:28 +0900

    Updated for eba90b8.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=003d64c3230697393da2342d6357e3a3d0085ca7
+COMMIT=eba90b805b38a1e6c57295a1d3a17dc86d862275
 wget https://github.com/pghvlaans/sbotools3/archive/$COMMIT.zip
