463400fce6f9993d75a15e3df53b398320ccab24
Gene Carlson <kvngncrlsn@gmail.com>
2024-11-04 02:51:41 +0900

    Updated for b750382.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=8e01929747ae0160d66cf342987d543063940ecd
+COMMIT=b750382d17d7fbd0a45edfa7b36f6e7e6af0af13
 wget https://github.com/pghvlaans/sbotools3/archive/$COMMIT.zip
