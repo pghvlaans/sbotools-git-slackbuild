42666ee221da2329525ec93a778a93427f6973e8
Gene Carlson <kvngncrlsn@gmail.com>
2024-11-04 03:06:53 +0900

    Updated for a32d303.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=b750382d17d7fbd0a45edfa7b36f6e7e6af0af13
+COMMIT=a32d303f72dbf14e9ea075e4c8ade76331341c60
 wget https://github.com/pghvlaans/sbotools3/archive/$COMMIT.zip
