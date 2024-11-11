975261cf5e3354b2762101234a3a0d2e7a6235ad
Gene Carlson <kvngncrlsn@gmail.com>
2024-11-11 07:59:33 -0500

    get-commit.sh: Reflect f14a7fd.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=0637cfb941a9546823f916ba3bee041f649b16ac
+COMMIT=f14a7fdf0ec1ef2eb44adcf5a228ffb57d68e4fb
 wget https://github.com/pghvlaans/sbotools3/archive/$COMMIT.zip
