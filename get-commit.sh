343bbf2160c1567104df2393daa000086dba14c5
Gene Carlson <kvngncrlsn@gmail.com>
2024-11-22 09:26:53 +0900

    Updated for e07be58.

--- get-commit.sh
+++ get-commit.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Download the latest commit from the master branch
-# of pghvlaans/sbotools3. Be sure to run "git pull"
+# of pghvlaans/sbotools. Be sure to run "git pull"
 # beforehand.
 
 cd $(dirname $0) || exit
 
-COMMIT=9690335420f2b48f116837fcb7099510a52af09a
-wget https://github.com/pghvlaans/sbotools3/archive/$COMMIT.zip
+COMMIT=e07be58fb4f347f02f67c43dc0922bf4ef95fe8f
+wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
