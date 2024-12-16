c6541b1c9124658c6fe2e81d70d2b896be0ed422
Gene Carlson <kvngncrlsn@gmail.com>
2024-12-16 22:43:23 +0900

    Updated for f76062d.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=4a099b1b71c0ac9c58967d55fede5aff222493c4
+COMMIT=f76062d42c6d117e8cf14ebc74161b3cdeea82d0
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
