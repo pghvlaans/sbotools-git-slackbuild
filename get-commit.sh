fa333fdccb120b7b916a04183b910c05a6dd1c74
Gene Carlson <kvngncrlsn@gmail.com>
2024-12-25 09:05:44 +0900

    Updated for 1aabbcf.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=2cefb0f02469908f1e22e5698684317c4e5f2a50
+COMMIT=1aabbcf9fd91441ecf277f85a2cef9e37d4dff8f
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
