51d76d85d997d85ed72de33e081b526e62be3a6f
Gene Carlson <kvngncrlsn@gmail.com>
2024-12-28 13:37:48 +0900

    Updated for 6c4a727.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=e719e3dbaed90c9df8aa86339610bd655ec3bbf7
+COMMIT=6c4a7277b03bd951fafc6bcc89d92b84a25675c2
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
