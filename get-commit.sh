6ee2fec1a71ddd63c2ab8e32e9df82d2976c29c9
Gene Carlson <kvngncrlsn@gmail.com>
2025-01-05 14:11:04 +0900

    Updated for f2cba9b.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=66d22e3ad85e24ffe2078ea369db72db939e6de7
+COMMIT=f2cba9b378cc59740afe69c108d9e20df0f2f6b2
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
