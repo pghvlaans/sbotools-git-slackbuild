59dda82eb032aede54efa3d0e6ede8f208604762
Gene Carlson <kvngncrlsn@gmail.com>
2025-01-25 13:53:11 +0900

    Updated for d761d1d.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=512410b02c7ecaac3e4b80c8f29887fc7046d275
+COMMIT=d761d1dfe0925209cf03845198622c17a0727028
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
