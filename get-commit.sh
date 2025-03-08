9126065d15b588dd5493a2d164f0dd0e818f7aef
Gene Carlson <kvngncrlsn@gmail.com>
2025-03-08 19:17:30 +0900

    Updated for ea63015.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=411d6f0d480245a7868e0ea6effda2274ba1a916
+COMMIT=ea6301510ca8b88c497a8a3c49d73482ba1c5da7
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
