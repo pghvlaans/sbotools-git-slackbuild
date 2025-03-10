b5936a261e63e035afa6523742f7d824b4603e83
Gene Carlson <kvngncrlsn@gmail.com>
2025-03-11 00:18:24 +0900

    Updated for 50d08b3.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=033fb695c97d119bceb476f6a0be4e9ac8040e51
+COMMIT=50d08b37b15f9a3b10a94ada6fbb8c03e90a703c
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
