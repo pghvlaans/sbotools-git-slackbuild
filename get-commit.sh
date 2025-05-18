5f37f4c2045c20c2082d380c13feb4b7dff5f184
Gene Carlson <kvngncrlsn@gmail.com>
2025-05-18 18:44:29 +0900

    Updated for c51a24d.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=5a0b45519456b4efa4d8be087c402dca267b02fd
+COMMIT=c51a24d1ebcf0ca3a011afa1c47bcb3c44e817db
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
