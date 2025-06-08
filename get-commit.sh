45dc3b029f295e62fe9f02e182f757ef035e985f
Gene Carlson <kvngncrlsn@gmail.com>
2025-06-08 22:14:34 +0900

    Updated for 4df6952.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=5c70b8c10ab3825c8ca20e1120b85c52530787a2
+COMMIT=4df6952f32a3e46d0bb4ea3937b6608afcc2a5f1
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
