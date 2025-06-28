1a65ee70cd3e86fd112689854abef6536bfcedeb
Gene Carlson <kvngncrlsn@gmail.com>
2025-06-28 15:12:45 +0900

    Updated for 3eae682.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=af6a866e8551af0b6e354e791c78f576d471fbd4
+COMMIT=3eae6827fff53acabcc440fe634ce49f920cf770
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
