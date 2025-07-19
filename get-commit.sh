c8ba04549640a3825bf6655842743b694d081726
Gene Carlson <kvngncrlsn@gmail.com>
2025-07-19 17:10:52 +0900

    Updated for 43cbeaf.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=343e999de0274a9962455ec35cf0539a2c417788
+COMMIT=43cbeaf306550df30bdb209d729b498eb3b5b83a
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
