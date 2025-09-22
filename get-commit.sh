8e197e16be2283ca2facc02c7e3852a74e1df7ec
Gene Carlson <kvngncrlsn@gmail.com>
2025-09-22 20:09:19 +0900

    Updated for e08db14.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=1916926d75fb5775204249fdf211fa71da58d1e9
+COMMIT=e08db143bf2645d96ee1e7195fe592bba42f53d3
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
