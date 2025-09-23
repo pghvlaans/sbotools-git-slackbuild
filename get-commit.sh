d50c20d790f3daef720047502ceef8b4c97caf47
Gene Carlson <kvngncrlsn@gmail.com>
2025-09-24 01:13:01 +0900

    Updated for aba4f7d.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=e08db143bf2645d96ee1e7195fe592bba42f53d3
+COMMIT=aba4f7de3bdae1e16cde5cbf4ecbd0cdb8e51da0
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
