20b663897ade058c0193d705b3d666274542ef00
Gene Carlson <kvngncrlsn@gmail.com>
2025-10-31 14:14:55 +0900

    Updated for b882bf0.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=d4cd7f665a95c5cf4abdc22eea47af4db4f64f23
+COMMIT=b882bf0dcfcdcf73b3176768b2ac9be2e3f6c10c
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
