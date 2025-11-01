7e4fc2d8abe17f32b311d5e8c7aac9472e676f7b
Gene Carlson <kvngncrlsn@gmail.com>
2025-11-01 14:59:23 +0900

    Updated for ddeae92.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=d404a84f921e91e96ac726e1642363c3ff43d16b
+COMMIT=ddeae92571e6dd3ad61aa43a8ef5bb1a010b8e01
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
