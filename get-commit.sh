23b5288d95216c144aebd0331276dec674abb06f
Gene Carlson <kvngncrlsn@gmail.com>
2025-11-01 15:41:03 +0900

    Updated for fbd996f.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=ddeae92571e6dd3ad61aa43a8ef5bb1a010b8e01
+COMMIT=fbd996f4945a1ae3a129cdbc1787074a140cc106
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
