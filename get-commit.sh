c6b31dd71aac8fd915de8e064b36b897a28dc383
Gene Carlson <kvngncrlsn@gmail.com>
2026-05-03 17:27:49 +0900

    Updated for cb20f09.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=8b3861aa289c635116f857bf91518bdcf6b49d5e
+COMMIT=cb20f0989de5a69951caba4356d5a3c4777abbc7
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
