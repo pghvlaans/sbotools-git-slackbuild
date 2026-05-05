97a1140a8add20fff04fe6bb26ea5dfe7144730d
Gene Carlson <kvngncrlsn@gmail.com>
2026-05-05 17:54:49 +0900

    Updated for 34face9.

--- get-commit.sh
+++ get-commit.sh
@@ -6,5 +6,5 @@
 
 cd $(dirname $0) || exit
 
-COMMIT=cb20f0989de5a69951caba4356d5a3c4777abbc7
+COMMIT=34face9250f005cfa06e99abac2f5e8c93ebf35e
 wget https://github.com/pghvlaans/sbotools/archive/$COMMIT.zip
